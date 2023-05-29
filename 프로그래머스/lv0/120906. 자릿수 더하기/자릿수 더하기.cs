b8d13877814425a577691f0dd3144324d88af704
koll2222 <70836139+koll2222@users.noreply.github.com>
2023-05-29 12:10:18 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.35 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        string tmp = n.ToString();
+        for(int i = 0; i < tmp.Length; i++)
+        {
+            answer += (int)tmp[i] - '0';
+        }
+        return answer;
+    }
+}
