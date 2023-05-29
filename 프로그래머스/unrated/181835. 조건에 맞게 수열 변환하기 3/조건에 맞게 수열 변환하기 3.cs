8f5e5ecc567f574a2ef4c29541d268eb5cc028b6
koll2222 <70836139+koll2222@users.noreply.github.com>
2023-05-29 11:48:44 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 0.74 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
@@ -4,19 +4,9 @@ public class Solution {
     public int[] solution(int[] arr, int k)
     {
         int[] answer = new int[arr.Length];
-        if(k % 2 == 1)
+        for(int i = 0; i < arr.Length; i++)
         {
-            for(int i = 0; i < arr.Length; i++)
-            {
-                answer[i] = arr[i] * k;
-            }
-        }
-        else
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                answer[i] = arr[i] + k;
-            }
+            answer[i] = k % 2 == 1 ? arr[i] * k : arr[i] + k;
         }
         return answer;
     }
