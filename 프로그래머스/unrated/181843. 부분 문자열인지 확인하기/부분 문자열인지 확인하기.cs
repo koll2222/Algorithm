9197d96a850f07be1a62ba252472e7354253db97
koll2222 <70836139+koll2222@users.noreply.github.com>
2023-05-29 11:57:54 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.21 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cs
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cs
@@ -2,11 +2,6 @@ using System;
 
 public class Solution {
     public int solution(string my_string, string target) {
-        int answer = 0;
-        if (my_string.Contains(target))
-        {
-            answer = 1;
-        }
-        return answer;
+        return my_string.Contains(target) ? 1 : 0;
     }
 }
