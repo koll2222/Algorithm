78a99dffaa4767fd3361ebb8eefe730be67d98e5
koll2222 <70836139+koll2222@users.noreply.github.com>
2023-06-05 11:50:43 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.86 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
@@ -4,10 +4,10 @@ using System.Linq;
 public class Solution {
     public int solution(int[] num_list) {
         int mul = 1;
-        int square = num_list.Sum() * num_list.Sum();
+        int sum = num_list.Sum() * num_list.Sum();
         foreach(int n in num_list)
             mul *= n;
         
-        return mul < square ? 1 : 0;
+        return mul < sum ? 1 : 0;
     }
 }
